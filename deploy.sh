50988f5ec67cb419d31047b09c384f0f4348c17b
mauliksoneji <maulik.soneji@go-jek.com>
2019-02-22 17:25:59 +0530

    [Maulik] Enable snapshot for all branch

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-  openssl aes-256-cbc -K
+if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+  openssl aes-256-cbc -K $encrypted_31978aceeffd_key -iv $encrypted_31978aceeffd_iv -in key.gpg.enc -out key.gpg -d
   ./gradlew uploadArchives -PossrhUsername=${SONATYPE_USERNAME} -PossrhPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=key.gpg
 fi
