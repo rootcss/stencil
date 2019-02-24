f5bbbd71d9aea389829dbbd270f5885107674140
mauliksoneji <maulik.soneji@go-jek.com>
2019-02-24 11:53:37 +0530

    [Maulik] Add print statement

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 openssl aes-256-cbc -K $encrypted_31978aceeffd_key -iv $encrypted_31978aceeffd_iv -in key.gpg.enc -out key.gpg -d
+echo "Deploying staging artifact"
 ./gradlew uploadArchives -PossrhUsername=${SONATYPE_USERNAME} -PossrhPassword=${SONATYPE_PASSWORD} -Psigning.keyId=${GPG_KEY_ID} -Psigning.password=${GPG_KEY_PASSPHRASE} -Psigning.secretKeyRingFile=key.gpg
